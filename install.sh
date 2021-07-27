daf545eb8a8fcdb36a8b2cf442dc7b6a328d53b8
Ren Baoshuo <i@baoshuo.ren>
2021-07-27 19:32:10 +0800

    Fix cron `bad username` error

--- install.sh
+++ install.sh
@@ -27,10 +27,12 @@ define CONFEDERATIONAS = $confederationas;
 
 echo '*** Write crontab configs to /etc/crontab ...'
 
-echo '0 * * * * wget -4 -O /tmp/bird.conf https://raw.githubusercontent.com/renbaoshuo/network-configs/master/bird.conf && mv /tmp/bird.conf /etc/bird/bird.conf && birdc c
-0 * * * * wget -4 -O /tmp/filter.conf https://raw.githubusercontent.com/renbaoshuo/network-configs/master/filter.conf && mv /tmp/filter.conf /etc/bird/filter.conf && birdc c
-0 * * * * wget -4 -O /tmp/ibgp.conf https://raw.githubusercontent.com/renbaoshuo/network-configs/master/ibgp.conf && mv /tmp/ibgp.conf /etc/bird/ibgp.conf && birdc c
+echo '0 * * * * root wget -4 -O /tmp/bird.conf https://raw.githubusercontent.com/renbaoshuo/network-configs/master/bird.conf && mv /tmp/bird.conf /etc/bird/bird.conf && birdc c
+0 * * * * root wget -4 -O /tmp/filter.conf https://raw.githubusercontent.com/renbaoshuo/network-configs/master/filter.conf && mv /tmp/filter.conf /etc/bird/filter.conf && birdc c
+0 * * * * root wget -4 -O /tmp/ibgp.conf https://raw.githubusercontent.com/renbaoshuo/network-configs/master/ibgp.conf && mv /tmp/ibgp.conf /etc/bird/ibgp.conf && birdc c
 ' >> /etc/crontab
+systemctl restart cron
+systemctl status cron
 
 echo '*** Creating /etc/bird/peers/ folder...'
 
